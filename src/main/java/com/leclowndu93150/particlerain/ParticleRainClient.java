f026e28b7a103a7350e91ee9741d123b7a3bdb88
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-01-12 21:53:37 +0100

    well i need to remove it now

--- src/main/java/com/leclowndu93150/particlerain/ParticleRainClient.java
+++ src/main/java/com/leclowndu93150/particlerain/ParticleRainClient.java
@@ -3,14 +3,13 @@ package com.leclowndu93150.particlerain;
 import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
 import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLLoader;
 
 @Mod(ParticleRainClient.MOD_ID)
-public class ParticleRainClient{
+public class ParticleRainClient {
 
     public static final String MOD_ID = "particlerain";
     public static int particleCount;
@@ -20,7 +19,7 @@ public class ParticleRainClient{
     public ParticleRainClient() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
         ParticleRegistry.register(modEventBus);
-        if(FMLLoader.getDist() == Dist.CLIENT) {
+        if (FMLLoader.getDist() == Dist.CLIENT) {
             AutoConfig.register(ModConfig.class, JanksonConfigSerializer::new);
             config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
             AutoConfig.getConfigHolder(ModConfig.class).registerSaveListener(ClientStuff::saveListener);
@@ -28,5 +27,4 @@ public class ParticleRainClient{
             ClientStuff.setupClient();
         }
     }
-
 }
