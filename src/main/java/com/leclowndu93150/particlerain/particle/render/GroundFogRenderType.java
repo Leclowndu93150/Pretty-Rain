7ff9625caf416a3151f1918784f53ef8d63ad7f7
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-01-13 18:34:46 +0100

    added custom fog render type

---
+++ src/main/java/com/leclowndu93150/particlerain/particle/render/GroundFogRenderType.java
@@ -0,0 +1,39 @@
+package com.leclowndu93150.particlerain.particle.render;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.BufferBuilder;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
+import com.mojang.blaze3d.vertex.Tesselator;
+import com.mojang.blaze3d.vertex.VertexFormat;
+import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.api.distmarker.OnlyIn;
+
+@OnlyIn(Dist.CLIENT)
+public class GroundFogRenderType implements ParticleRenderType {
+    public static final ParticleRenderType INSTANCE = new GroundFogRenderType();
+
+    @Override
+    public BufferBuilder begin(Tesselator tesselator, TextureManager textureManager) {
+        RenderSystem.depthMask(false);
+        RenderSystem.enableBlend();
+        RenderSystem.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+        RenderSystem.setShader(GameRenderer::getParticleShader);
+        RenderSystem.setShaderTexture(0, TextureAtlas.LOCATION_PARTICLES);
+        return tesselator.begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.PARTICLE);
+    }
+
+    @Override
+    public String toString() {
+        return "particlerain:ground_fog";
+    }
+
+    @Override
+    public boolean isTranslucent() {
+        return true;
+    }
+}
