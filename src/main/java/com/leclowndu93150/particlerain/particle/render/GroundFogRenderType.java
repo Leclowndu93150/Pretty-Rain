34806dfd1de9042cffbf87574018bc1a398b3673
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-04-13 18:12:49 +0200

    Not worth a release imo

--- src/main/java/com/leclowndu93150/particlerain/particle/render/GroundFogRenderType.java
+++ src/main/java/com/leclowndu93150/particlerain/particle/render/GroundFogRenderType.java
@@ -1,18 +1,42 @@
 package com.leclowndu93150.particlerain.particle.render;
 
+import com.mojang.blaze3d.pipeline.BlendFunction;
+import com.mojang.blaze3d.pipeline.RenderPipeline;
+import com.mojang.blaze3d.platform.DestFactor;
+import com.mojang.blaze3d.platform.SourceFactor;
 import net.minecraft.client.particle.ParticleRenderType;
+import net.minecraft.client.renderer.RenderPipelines;
+import net.minecraft.client.renderer.RenderStateShard;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.texture.TextureAtlas;
-import net.neoforged.api.distmarker.Dist;
-import net.neoforged.api.distmarker.OnlyIn;
+import net.minecraft.util.TriState;
 
-@OnlyIn(Dist.CLIENT)
+import static net.minecraft.client.renderer.RenderPipelines.PARTICLE_SNIPPET;
+import static net.minecraft.client.renderer.RenderStateShard.*;
+
 public class GroundFogRenderType {
-    // In 1.21.5, ParticleRenderType is a record with name, renderType, and translucent fields
-    // We simply create an instance of this record with the appropriate values
+    public static final BlendFunction FOG_BLEND = new BlendFunction(SourceFactor.SRC_ALPHA, DestFactor.ONE_MINUS_SRC_ALPHA);
+    public static final RenderPipeline FOG_PIPELINE = RenderPipelines.register(
+            RenderPipeline.builder(PARTICLE_SNIPPET)
+                    .withLocation("pipeline/particlerain_fog")
+                    .withBlend(FOG_BLEND)
+                    .withDepthWrite(false).build()
+    );
+    private static final RenderType fog = RenderType.create(
+            "particlerain_fog",
+            1536,
+            false,
+            false,
+            FOG_PIPELINE,
+            RenderType.CompositeState.builder()
+                    .setTextureState(new RenderStateShard.TextureStateShard(TextureAtlas.LOCATION_PARTICLES, TriState.FALSE, false))
+                    .setOutputState(PARTICLES_TARGET)
+                    .setLightmapState(LIGHTMAP)
+                    .createCompositeState(false)
+    );
+
     public static final ParticleRenderType INSTANCE = new ParticleRenderType(
-            "particlerain:ground_fog",
-            RenderType.translucentParticle(TextureAtlas.LOCATION_PARTICLES),
-            true
+            "particlerain:fog",
+            fog
     );
 }
